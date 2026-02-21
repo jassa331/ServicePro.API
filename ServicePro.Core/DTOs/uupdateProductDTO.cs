6ebd4971dbdb05904e3f28b11245b61d83d977d4
jassa331 <jassadhammi@gmail.com>
2026-02-21 09:29:24 -0800

    Update product image update logic with Cloudinary delete support

---
+++ ServicePro.Core/DTOs/uupdateProductDTO.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicePro.Core.DTOs
+{
+    public class UpdateSingleImageDTO
+    {
+        public Guid ProductId { get; set; }
+        public Guid ProductImageId { get; set; }
+        public IFormFile NewImage { get; set; }
+    }
+    public class uupdateProductDTO
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public List<IFormFile>? Images { get; set; }
+    }
+}
